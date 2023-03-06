0302bab4dca1a52b4450eb0d32b04dcaa49b6d7e
sachieko <arashikabane@gmail.com>
2023-03-06 04:07:05 -0700

    Add optional status text and message to error interface

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,8 +1,8 @@
 import { useRouteError } from "react-router-dom";
 
 interface IError {
-  statusText: string;
-  message: string;
+  statusText?: string;
+  message?: string;
 }
 
 export default function ErrorPage() {
