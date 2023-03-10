c8c4a768a75a3af8dd01a100fec10120eef73154
sachieko <arashikabane@gmail.com>
2023-03-10 01:53:19 -0700

    Add error handling to destroy action

--- src/main.tsx
+++ src/main.tsx
@@ -36,6 +36,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>
       },
     ]
   },
