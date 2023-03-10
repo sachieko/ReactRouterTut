52ccbba82353fa9cd9eca67b52993f80ef84e978
sachieko <arashikabane@gmail.com>
2023-03-10 02:52:25 -0700

    Implement optimistic ui for favorite button

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -84,6 +84,9 @@ function Favorite({ contact }: IFavorite) {
   const fetcher = useFetcher();
   // yes, this is a `let` for later
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
     <fetcher.Form method="post">
       <button
