4172865a0b1c29c56d0465aefcdbe9ab941ca4f4
sachieko <arashikabane@gmail.com>
2023-03-10 03:01:11 -0700

    Final commit, project is in finished state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ export default function Index() {
   return (
     <p id="zero-state">
       This is a demo for React Router, but using typescript!
+      This was created by Sachieko, and I likely made a few mistakes or took the same shortcuts that the tutorial did.
+      Thanks for checking out this project!
       <br />
       Check out {" "}
       <a href="https://reactrouter.com">
