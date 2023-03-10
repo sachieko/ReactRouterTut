c396655dc73e42b595b6c873d171a7657d92a35a
sachieko <arashikabane@gmail.com>
2023-03-10 01:34:27 -0700

    add navlink component to display active and pending contact links

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,6 @@
 import {
   Outlet,
+  NavLink,
   Link,
   useLoaderData,
   Form,
@@ -52,6 +53,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                   <Link to={`contacts/${contact.id}`}>
                     {contact.first || contact.last ? (
                       <>
@@ -62,6 +73,7 @@ export default function Root() {
                     )}{" "}
                     {contact.favorite && <span>★</span>}
                   </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
